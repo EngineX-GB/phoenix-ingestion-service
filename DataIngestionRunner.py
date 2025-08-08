a7b9386d2fc04bf38598cf185c100c20a9258b11
EngineX-GB <uk.engine.x@gmail.com>
2025-08-08 16:57:29 +0100

    Updated runner with argument handling

--- DataIngestionRunner.py
+++ DataIngestionRunner.py
@@ -1,23 +1,25 @@
 from DataAccessImpl import DataAccessImpl
+import sys
 
 if __name__ == "__main__":
     dataAccess = DataAccessImpl()
-    feed_files_list = ['feeds/clients_2025-08-07_130951.txt',
-                       'feeds/clients_2025-08-07_131023.txt',
-                       'feeds/clients_2025-08-07_131058.txt',
-                       'feeds/clients_2025-08-07_131129.txt',
-                       'feeds/clients_2025-08-07_131158.txt',
-                       'feeds/clients_2025-08-07_131228.txt',
-                       'feeds/clients_2025-08-07_131256.txt',
-                       'feeds/clients_2025-08-07_131326.txt',
-                       'feeds/clients_2025-08-07_131355.txt',
-                       'feeds/clients_2025-08-07_131422.txt',
-                       'feeds/clients_2025-08-07_131449.txt',
-                       'feeds/clients_2025-08-07_131514.txt',
-                       'feeds/clients_2025-08-07_131535.txt',
-                       'feeds/clients_2025-08-07_131602.txt',
-                       'feeds/clients_2025-08-07_131618.txt'
-                       ]
-    #dataAccess.load_feed_data(feed_files_list)
 
-    dataAccess.load_feed_data_by_directory("C:/Users/Dell/PycharmProjects/data-ingestion/feeds")
+    if len(sys.argv) == 1:
+        print("[ERROR] Invalid number of parameters. Type --help for more information")
+        exit(1)
+    if sys.argv[1] == "--help":
+        print("[INFO] Help Manual added here")
+        exit(0)
+    if sys.argv[1] == "--service":
+        print("[INFO] Running Ingestion Service....")
+        print("[WARN] This feature is not supported yet.")
+        exit(0)
+    if sys.argv[1] == "--cmd":
+        print("[INFO] Running Ingestion Service....")
+        if sys.argv[2] == "--batch":
+            folder_path = sys.argv[3]
+            dataAccess.load_feed_data_by_directory(folder_path)
+        elif sys.argv[2] == "--custom-load":
+            feed_file_list_string = sys.argv[3]
+            feed_file_list = feed_file_list_string.split(",")
+            dataAccess.load_feed_data(feed_file_list)
