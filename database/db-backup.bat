0947a16d2e0272cd74e630c6c156e3930d3dfd7f
EngineX-GB <uk.engine.x@gmail.com>
2026-02-26 14:11:18 +0000

    Added a backup script

--- database/db-backup.bat
+++ database/db-backup.bat
@@ -2,3 +2,6 @@
 SET MYSQL_HOME="C:\Program Files\MySQL\MySQL Server 8.3\bin"
 %MYSQL_HOME%\mysqldump -uroot -proot --routines --triggers --events db_phoenix > db_phoenix_backup_260226.sql
 @echo on
+
+rem restore the database:
+rem %MYSQL_HOME%\mysql -uroot -proot < db_phoenix_backup_260226.sql
